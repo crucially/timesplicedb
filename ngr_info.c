3fb019f6403f59e70426d97b4be4be9e3730ebd4
Artur <sky@crucially.net>
2009-11-12 15:44:22 -0800

    ngrinfo working

--- ngr_info.c
+++ ngr_info.c
@@ -3,9 +3,36 @@
 #include <sys/time.h>
 #include <stdio.h>;
 #include <time.h>;
+#include <assert.h>;
+#include <string.h>
 
-int main() {
-  struct NGR_metric_t *metric = NGR_open("foo", "data1");
+#include <stdlib.h>;
+
+extern char *optarg;
+
+int main(int argc, char * const *argv) {
+  int o;
+  char *collection, *metric_s;
+
+  while ((o = getopt(argc, argv,
+		     "c:m:")) != -1) {
+
+    switch(o) {
+    case 'c':
+      collection = malloc(strlen(optarg)+1);
+      memcpy(collection, optarg, strlen(optarg)+1);
+      break;
+    case 'm':
+      metric_s = malloc(strlen(optarg)+1);
+      memcpy(metric_s, optarg, strlen(optarg)+1);
+      break;
+    }
+  }
+  
+  assert(collection);
+  assert(metric_s);
+
+  struct NGR_metric_t *metric = NGR_open(collection, metric_s);
   time_t last_entry = (metric->created + (NGR_last_entry_idx(metric) * metric->resolution));
 
   printf("Starting time: %s", ctime(&(metric->created)));
